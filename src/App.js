68472828b699d4bfd398a71f1ffd0fd6f9ebec7b
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-23 18:41:27 +0500

    live time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hours , setHours] = useState();
+  
+  useEffect(()=>{
+    setInterval(()=>{
+      const date = new Date();
+      setHours(date.toLocaleTimeString());
+    },1000);
+  },[]);
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <h1>{hours}</h1>
+    
   );
 }
 
